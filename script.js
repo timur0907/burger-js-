0ce3d62f4e262fe5fe39e13b120b44c980f6a1e5
timur0907 <timurabdulvohidov@gmail.com>
2022-01-17 11:41:45 +0500

    second

--- script.js
+++ script.js
@@ -57,19 +57,17 @@ const extraProduct = {
 
 const btnPlusOrMinus = document.querySelectorAll('.main__product-btn'),
       checkExtraProduct = document.querySelectorAll('.main__product-checkbox');
-        // console.log(btnPlusOrMinus);
+        
       
     for(let i = 0; i < btnPlusOrMinus.length; i++) {
         btnPlusOrMinus[i].addEventListener('click', function(){
             plusOrMinus(this);
         })
-        // console.log(btnPlusOrMinus[i]);
+       
     }
 
 function plusOrMinus(element) {
-    // console.log(element);
-    // .closest() - метод возвращает ближайший родительский элемент. Если совпадений нету null
-    // .getAttribute() - возвращает значение указанного атрибута
+    
     const parent = element.closest('.main__product'),
           parentId = parent.getAttribute('id'),
           amount =  parent.querySelector('.main__product-num'),
@@ -92,14 +90,14 @@ function plusOrMinus(element) {
 }
 // ---------------доп.продукция----------
 function addExtraProduct(element) {
-    // console.log(element);
+  
     const parent = element.closest('.main__product'),
           parentId = parent.getAttribute('id'),
           price = parent.querySelector('.main__product-price span'),
           kcall = parent.querySelector('.main__product-kcall span'),
           elementExtra = element.getAttribute('data-extra');
           product[parentId][elementExtra] = element.checked; 
-        //   console.log(product[parentId]); 
+        
         if(product[parentId][elementExtra] === true){
            product[parentId].price += extraProduct[elementExtra].price; 
            product[parentId].kcall += extraProduct[elementExtra].kcall; 
@@ -138,10 +136,8 @@ let arrayProduct = [],
         const po = product[key];
         if(po.amount > 0) {
             arrayProduct.push(po);
-            // console.log(arrayProduct);
             for(const newKey in po){
                 if(po[newKey] === true){
-                    // '\n' - это экранирование, означает перенос строки
                     po.name += '\n' + extraProduct[newKey].name
                 }
             }
@@ -150,14 +146,12 @@ let arrayProduct = [],
         po.kcall = po.Kcall;
       }
       
-    //   console.log(arrayProduct);
       for(let i = 0; i < arrayProduct.length; i++) {
           const obj = arrayProduct[i];
                 totalPrice += obj.price;
                 totalKcall += obj.kcall;
                 totalName += '\n' + obj.name + '\n';  
         }
-    //   console.log(totalKcall);
       receiptOut.innerHTML = `Вы заказали: \n ${totalName} \n Калорийность ${totalKcall} \n Стоимость заказа:  ${totalPrice} сум`;
       receipt.style.display = 'flex';
       setTimeout(function(){
@@ -170,4 +164,3 @@ let arrayProduct = [],
   receiptBtn.addEventListener('click', function () {
       location.reload();
   })
-//   console.log('Тимур это перенос \n строки' );
